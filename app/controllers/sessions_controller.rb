d88a8fcd9eda12640a2f0d732364562b67ec0f14
guilleiguaran <guilleiguaran@gmail.com>
2011-02-10 00:22:13 -0500

    Showing User Info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
   def create
-    raise request.env["omniauth.auth"].to_yaml
+    render :text => request.env["omniauth.auth"].to_yaml.to_s.gsub("\n","<br/>")
   end
 end
