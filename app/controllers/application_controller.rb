4e8bcfddf777c2becc7fae91167ea600fdb0411a
guilleiguaran <guilleiguaran@gmail.com>
2011-02-10 00:09:52 -0500

    Authentication for staging server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate_for_staging if Rails.env.staging?
+
+  def authenticate_for_staging
+    authenticate_or_request_with_http_basic do |username, password|
+        username == "m3b" && password == "m3b-staging"
+    end
+  end
 end
