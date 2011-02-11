80e45f53f2072be764cbd09ad5324eab7363d319
guilleiguaran <guilleiguaran@gmail.com>
2011-02-10 21:53:13 -0500

    Renaming staging authenticate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_for_staging if Rails.env.staging?
+  before_filter :authenticate if Rails.env.staging?
 
-  def authenticate_for_staging
+  def authenticate
     authenticate_or_request_with_http_basic do |username, password|
         username == "m3b" && password == "m3b-staging"
     end
