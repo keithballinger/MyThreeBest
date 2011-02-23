f8c8ffb0554fc16bf19e93f728fc8e131496a5a2
guilleiguaran <guilleiguaran@gmail.com>
2011-02-23 00:37:51 -0500

    Fixing UserPhotos job

--- app/jobs/user_photos.rb
+++ app/jobs/user_photos.rb
@@ -7,13 +7,13 @@ class UserPhotos < Resque::JobWithStatus
     graph = Koala::Facebook::GraphAPI.new(user.facebook_token)
     photos = graph.get_connections("me", "photos")
     total = photos.count
-    num = 0
+    num = 1
     photos.each do |photo|
       at(num, total, "Looking at photo #{num} of #{total}")
-      preview = photos["images"].select{|x| x["width"] == 180}.first["source"]
-      url = photos["images"].first["source"]
-      title = photos["name"]
-      Photo.create(:title => title, :preview_url => preview, :url => url, :user_id => user.id)
+      preview = photo["images"].select{|x| x["width"] == 180}.first["source"]
+      url = photo["images"].first["source"]
+      title = photo["name"]
+      Photo.create!(:title => title, :preview_url => preview, :url => url, :user_id => user.id)
       num += 1
     end
     user.user_photos_job.update_attributes(:status => "completed")
