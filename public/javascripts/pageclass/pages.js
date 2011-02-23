39a08c4aa02227a9eb0c149237d2387e75fcc6e0
Keith Ballinger <keithba@gmail.com>
2011-02-23 14:40:24 -0500

    initial jquery load of friends

--- public/javascripts/pageclass/pages.js
+++ public/javascripts/pageclass/pages.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-    window.reloadPath = "/invites/new";
-    window.reloadInterval = window.setInterval(reloadFriends,3000);
+    $.getJSON('http://localhost:3000/friends/list', function(data) {
+      var items = [];
+
+      $.each(data.friends, function(key, val) {
+        items.push('<li id="user' + key + '"><img src="' + val.user.profile_picture + '" />' + val.user.first_name + ' ' + val.user.last_name + '</li>');
+      });
+
+      $('<ul/>', {
+        'class': 'my-new-list',
+        html: items.join('')
+      }).appendTo('#friends');
+      
+      $('#loading').hide();
+      
+    });
 });
