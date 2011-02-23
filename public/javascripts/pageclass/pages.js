759ef538fce34201abc358265522fb701b30e1e4
Keith Ballinger <keithba@gmail.com>
2011-02-23 15:04:59 -0500

    jquery loading of friends with job_status checking

--- public/javascripts/pageclass/pages.js
+++ public/javascripts/pageclass/pages.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
-    $.getJSON('http://localhost:3000/friends/list', function(data) {
-      var items = [];
+    getFriends();
+});
 
-      $.each(data.friends, function(key, val) {
+function getFriends(){
+    $.getJSON('http://localhost:3000/friends/list', function(response) {
+        
+        if( response.job_status == "completed" ){
+            loadFriends(response.friends);
+        }else{
+            setInterval("getFriends()", 1000);
+        }
+        
+    });
+}
+
+function loadFriends(friends){
+    var items = [];
+
+    $.each(friends, function(key, val) {
         items.push('<li id="user' + key + '"><img src="' + val.user.profile_picture + '" />' + val.user.first_name + ' ' + val.user.last_name + '</li>');
-      });
+    });
 
-      $('<ul/>', {
+    $('<ul/>', {
         'class': 'my-new-list',
         html: items.join('')
-      }).appendTo('#friends');
-      
-      $('#loading').hide();
-      
-    });
-});
+    }).appendTo('#friends');
+  
+    $('#loading').hide();
+}
