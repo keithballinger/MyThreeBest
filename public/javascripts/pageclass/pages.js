2e0301383270cd647854afcba613d2306264f914
Keith Ballinger <keithba@gmail.com>
2011-02-25 23:40:00 -0500

    switch over invite page to friends view model using knockout.js

--- public/javascripts/pageclass/pages.js
+++ public/javascripts/pageclass/pages.js
@@ -2,26 +2,12 @@ $(document).ready(function() {
     getFriends();
     window.getFriendsInterval = window.setInterval(getFriends, 1000);
     
-    var friendsViewModel = {
-        left_row: ko.observableArray([
-            { vote_id: "1", name: "Keith Ballinger", profile_image_url: "http://profile.ak.fbcdn.net/hprofile-ak-snc4/173926_100002115969383_4889585_q.jpg" },
-            { vote_id: "1", name: "Bob Kruger", profile_image_url: "http://profile.ak.fbcdn.net/hprofile-ak-snc4/173926_100002115969383_4889585_q.jpg" }
-        ]),
-        right_row: ko.observableArray([
-            { vote_id: "1", name: "Amy Bond", profile_image_url: "http://profile.ak.fbcdn.net/hprofile-ak-snc4/173926_100002115969383_4889585_q.jpg" },
-            { vote_id: "1", name: "James Smith", profile_image_url: "http://profile.ak.fbcdn.net/hprofile-ak-snc4/173926_100002115969383_4889585_q.jpg" }
-        ])
-
-    };
-
-    ko.applyBindings(friendsViewModel);
-    
 });
 
 function getFriends(){
-    $.getJSON('/friends', function(response) {
+    $.getJSON('/friends/list', function(response) {
         if( response.job_status == "completed" ) {
-            loadFriends(response.friends);
+            loadFriends(response);
             window.clearInterval(window.getFriendsInterval);
         }
     });
@@ -29,9 +15,18 @@ function getFriends(){
 
 
 
-function loadFriends(friends){
-    $( "#friend_template" ).tmpl(friends).appendTo("#friends_list");
+function loadFriends(response){
+    //$( "#friend_template" ).tmpl(friends).appendTo("#friends_list");
+    
+    var friendsViewModel = {
+        left_row: ko.observableArray(response.left_row),
+        right_row: ko.observableArray(response.right_row)
+    };
+
+    ko.applyBindings(friendsViewModel);
+    
     $('#loading').hide();
+    
 }
 
 
