652f7d8e602de168f1fb0c18371b9f2ce60b75e1
guilleiguaran <guilleiguaran@gmail.com>
2011-03-01 00:26:47 -0500

    Getting user profile photos

--- app/jobs/user_photos.rb
+++ app/jobs/user_photos.rb
@@ -6,16 +6,9 @@ class UserPhotos < Resque::JobWithStatus
     user.user_photos_job.update_attributes(:status => "working")
     graph = Koala::Facebook::GraphAPI.new(user.facebook_token)
     photos = []
-    partial_photos = graph.get_connections("me", "photos")
-    old_photo_count = 0
-    photo_count = nil
-    until old_photo_count == photo_count
-      old_photo_count = photos.size
-      photos = photos + partial_photos
-      photos.uniq!
-      photo_count = photos.size
-      partial_photos = partial_photos.next_page
-    end
+    photos = photos + get_photos(graph, "me")
+    profile_pics_album = graph.get_connections("me", "albums").select{|x| x["name"] == "Profile Pictures"}.first["id"] rescue nil
+    photos = photos + get_photos(graph, profile_pics_album)
     photos.uniq!
     total = photos.count
     num = 1
@@ -31,4 +24,20 @@ class UserPhotos < Resque::JobWithStatus
     completed
   end
 
+  def get_photos(graph, object)
+    return [] if object.nil?
+    photos = []
+    partial_photos = graph.get_connections(object, "photos")
+    return partial_photos if partial_photos.next_page.nil?
+    old_photo_count = 0
+    photo_count = nil
+    until old_photo_count == photo_count
+      old_photo_count = photos.size
+      photos = photos + partial_photos
+      photos.uniq!
+      photo_count = photos.size
+      partial_photos = partial_photos.next_page
+    end
+    return photos.uniq
+  end
 end
