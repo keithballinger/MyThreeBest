8c8e644d5b48fce61d4de87718da7754b77b5c80
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-03-20 12:46:14 -0500

    Including helpers in request tests

--- spec/request/support/helpers.rb
+++ spec/request/support/helpers.rb
@@ -2,8 +2,9 @@ module AcceptanceHelperMethods
   # Put helper methods you need to be available in all tests here.
 
   def create_user
-    user = User.create_with_omniauth(OmniAuth.config.mock_auth[:facebook].merge("uid" => rand(1000000000)))
+    #user = User.create_with_omniauth(OmniAuth.config.mock_auth[:facebook].merge("uid" => rand(1000000000)))
+    user = Factory.create(:registered_user)
   end
 end
 
-RSpec.configuration.include AcceptanceHelperMethods, :type => :acceptance
+RSpec.configuration.include AcceptanceHelperMethods, :type => :request
