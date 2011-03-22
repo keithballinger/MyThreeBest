9d8a9350ec16a46306c07720327ee61e3c414ccd
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-03-21 23:55:10 -0500

    Adding invite_email method and mailer views

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,3 +1,10 @@
 class InviteMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "no-reply@mythreebest.com"
+
+  def invite_email(inviter, invited, email)
+    @inviter = inviter.full_name
+    @invited = invited.first_name
+    @url  = inviter.public_page_url
+    mail(:to => email, :subject => "Vote My Three Best Photos!!")
+  end
 end
