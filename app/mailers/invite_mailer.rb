f2485ba95507fdd47c7da5f2d4167f5e4c2aa05d
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-03-22 00:11:10 -0500

    Refactoring invite mailer, adding spec

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -4,7 +4,7 @@ class InviteMailer < ActionMailer::Base
   def invite_email(inviter, invited, email)
     @inviter = inviter.full_name
     @invited = invited.first_name
-    @url  = inviter.public_page_url
+    @url = new_vote_url(inviter.id, :host => "m3b-staging.heroku.com")
     mail(:to => email, :subject => "Vote My Three Best Photos!!")
   end
 end
--- spec/mailers/invite_mailer_spec.rb
+++ spec/mailers/invite_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
 describe InviteMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should send a invitation email" do
+    inviter = Factory.create(:registered_user)
+    invited = Factory.create(:registered_user)
+    email = InviteMailer.invite_email(inviter, invited, "user@facebook.com").deliver
+
+    ActionMailer::Base.deliveries.should_not be_empty
+    email.to.should == ["user@facebook.com"]
+    email.subject.should == "Vote My Three Best Photos!!"
+    email.encoded.should match(/#{invited.first_name}/)
+    email.encoded.should match(/\/vote\/#{inviter.id}/)
+  end
 end
