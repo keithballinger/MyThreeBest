2fcead0edffa03b988959f66f1b78f6d2a2d81eb
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-04-09 10:48:14 -0500

    Better error handling for FriendsListJob

--- app/jobs/friends_list.rb
+++ app/jobs/friends_list.rb
@@ -11,13 +11,19 @@ class FriendsList < Resque::JobWithStatus
     friends.each do |friend|
       at(num, total, "Looking at user #{num} of #{total}")
       user = User.find_by_facebook_uid(friend['id'])
-      unless user
+      if user
+        me.friend(user) unless me.friend?(user.facebook_uid)
+      else
         info = graph.get_object(friend['id'])
         photo = graph.get_picture(friend['id'])
-        user = User.create!(:first_name => info['first_name'], :last_name => info['last_name'],
-                            :facebook_uid => friend['id'], :profile_picture => photo)
+        begin
+          user = User.create!(:first_name => info['first_name'], :last_name => info['last_name'],
+                           :facebook_uid => friend['id'], :profile_picture => photo)
+          me.friend(user) unless me.friend?(user.facebook_uid)
+        rescue
+          Rails.logger.error("Failed to save user: #{user.inspect}")
+        end
       end
-      me.friend(user) unless me.friend?(user.facebook_uid)
       num += 1
     end
     me.friends_list_job.update_attributes(:status => "completed")
