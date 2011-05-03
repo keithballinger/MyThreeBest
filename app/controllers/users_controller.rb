f98be7586187ea1275fc22d8921cc1a73d8caba4
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-05-02 22:15:57 -0500

    Public page is really public

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => :show
 
   def index
     @job = current_user.friends_list_job
