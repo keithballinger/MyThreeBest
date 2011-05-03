609770b48305f4f8438d50a91748f73fab0ff98d
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-05-02 22:11:16 -0500

    Disabling password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate if Rails.env.staging?
+  #before_filter :authenticate if Rails.env.staging?
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
