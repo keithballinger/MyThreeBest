422c176dc3e6870ec7047b96993ac2487e754fb6
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-05-03 00:37:21 -0500

    Detecting if Facebook is referrer

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -18,7 +18,7 @@ class VotesController < ApplicationController
   end
 
   def new
-    if params[:token]
+    if params[:token] && !request.env["HTTP_REFERER"].include?("facebook.com")
       @user = User.find(params[:user_id])
       respond_to do |format|
         format.html{ render :facebook, :layout => false }
