aec6d89a12a664d7ac76c2a90530c304d5e47608
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-06-02 20:36:07 -0500

    One more try to detect Facebook bot

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
   def index
+    Rails.logger.info "*"*50
+    Rails.logger.info request.env['HTTP_USER_AGENT']
+    Rails.logger.info "*"*50
     if user_signed_in?
       @friends = current_user.votes.includes(:voter).order("created_at desc").limit(18).map(&:voter).uniq[0..6]
       render :dashboard
