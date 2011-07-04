890f9967a412089737f840de194b89cf7160e28a
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-07-04 16:28:47 -0500

    try/catch for handle errors when ContentFlow is loading

--- public/javascripts/pageclass/votes.js
+++ public/javascripts/pageclass/votes.js
@@ -3,18 +3,20 @@ var photosPage = 1;
 var loadInterval;
 
 function loadPage() {
-    if(fantasticFlow.getNumberOfItems() - fantasticFlow.getActiveItem().index < 10) {
-        getPhotos(photosPage+1);
-    }
+    try {
+        if(fantasticFlow.getNumberOfItems() - fantasticFlow.getActiveItem().index < 10) {
+            getPhotos(photosPage+1);
+        }
+    } catch(err) { }
 }
 
-function getPhotos(page){
+function getPhotos(page) {
     $.getJSON(window.location.pathname+"?page="+page, function(response) {
         loadPhotos(response);
     });
 }
 
-function loadPhotos(response){
+function loadPhotos(response) {
     var flowChanged = false;
     $.each(response, function(index, object) {
         var photo = object.photo;
