13845ead20a1324b0e5d2fc1a086f6af7053fde7
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-09-06 01:51:07 -0500

    Fix AssetDigestHelper

--- lib/asset_digest_helper.rb
+++ lib/asset_digest_helper.rb
@@ -1,4 +1,6 @@
 module AssetDigestHelper
+  extend ActiveSupport::Concern
+
   # asset_digest("logo.png")       #=> "logo-bd9ad5a560b5a3a7be0808c5cd76a798.png"
   # asset_digest("application.js") #=> "application-b01eded31fd08af304f7da4bd2d2c505.js"
   # asset_digest("style.css")      #=> "style-8af74128f904600e41a6e39241464e03.css"
@@ -7,4 +9,10 @@ module AssetDigestHelper
   end
 end
 
-Sprockets::Helpers::RailsHelper.send(:include, AssetDigestHelper)
+ActiveSupport.on_load(:action_view) do
+  include AssetDigestHelper
+
+  Rails.application.assets.context_class.instance_eval do
+    include AssetDigestHelper
+  end
+end
